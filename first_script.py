9bea7d5ccb3278c9f32654ea4cc170aae7a4ab03
AJ Raymond <antraymo@iu.edu>
2020-08-14 13:52:52 -0500

    Lesson 6: scripting: first script

---
+++ first_script.py
@@ -0,0 +1,19 @@
+## Up to this point, the Udacity course provided a sandbox environment through their
+# program to work in. This is the first time it encouraged me to use my own text editor
+#  even though I've been using it the whole course
+how_many_snakes = 1
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Juno
+"""
+
+
+print(snake_string * how_many_snakes)
